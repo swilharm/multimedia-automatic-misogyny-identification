40db7bcd0a7d8d4e888f3973af73729e9a85f72d
Nellie <66431816+nelliemirz@users.noreply.github.com>
2022-03-19 23:11:24 +0100

    list of imports
    TODO: create a list of requirements

---
+++ image_classifier/modules.py
@@ -0,0 +1,21 @@
+import argparse
+import zipfile
+import os
+import cv2
+import time
+import copy
+import pandas as pd
+import numpy as np
+from pandas.core.common import index_labels_to_array
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+from torchvision.io import read_image
+from torch.utils.data import Dataset, DataLoader
+from torchvision import *
+import torchvision.transforms as transform
+import matplotlib.pyplot as plt
+import PIL.Image as Image
+
