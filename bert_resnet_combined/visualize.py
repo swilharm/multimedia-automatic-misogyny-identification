ccb75dd569c9f747ceacfe8847ab54206a110dae
Nellie <66431816+nelliemirz@users.noreply.github.com>
2022-03-22 14:12:16 +0100

    Update visualize.py

--- bert_resnet_combined/visualize.py
+++ bert_resnet_combined/visualize.py
@@ -1,7 +1,8 @@
 from modules import *
-from read_dataset import prepare_data
+from read_dataset import prepare_data, transform_data
 
-use_cuda = torch.cuda.is_available()
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+()
 _, test_dataloader = prepare_data(batch_size=16)
 
 ckp_path = "./wide_resnet.pt"
@@ -11,11 +12,15 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.0005, momentum=0.9)
 num_ftrs = net.fc.in_features
 net.fc = nn.Linear(num_ftrs, 256)
-net.fc = net.fc.cuda() if use_cuda else net.fc
+net.fc = net.fc.cuda() if device else net.fc
 
 trained_model = torch.load(ckp_path)
 net.load_state_dict(trained_model)
+net = net.cuda() if device else net
 net.eval()
+
+
+train_dataset, test_dataset = transform_data()
 
 def visualize_model(net, num_images=4):
     images_so_far = 0
@@ -23,24 +28,25 @@ def visualize_model(net, num_images=4):
     
     for i, data in enumerate(test_dataloader):
         inputs, labels = data
-        if use_cuda:
+        if device:
             inputs, labels = inputs.cuda(), labels.cuda()
         outputs = net(inputs)
         _, preds = torch.max(outputs.data, 1)
-        preds = preds.cpu().numpy() if use_cuda else preds.numpy()
+        preds = preds.cpu().numpy() if device else preds.numpy()
         for j in range(inputs.size()[0]):
             images_so_far += 1
             ax = plt.subplot(2, num_images//2, images_so_far)
             ax.axis('off')
             ax.set_title('prediction: {}'.format(test_dataset.labels[preds[j]]))
-            inputs = inputs.cpu() 
-            imshow(inputs[j])
+            inputs = inputs.cpu()
+            
+            imshow(inputs[j].permute(1, 2, 0))
             
             if images_so_far == num_images:
-                return 
+                return plt.show()
 
 
-if name == __main__():
+if __name__ == "__main__":
 
     plt.ion()
     visualize_model(net)
